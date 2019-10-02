e62c17ff2801fab30f1ed93d8c801d025bc1107d
Bea Mulvey <22732775+bmulvey1@users.noreply.github.com>
2019-10-02 14:59:46 -0400

    send notification for individual controller, run continuously

--- main.py
+++ main.py
@@ -11,19 +11,41 @@ for i in range(openvr.k_unMaxTrackedDeviceCount):
 leftId = openvr.VRSystem().getTrackedDeviceIndexForControllerRole(openvr.TrackedControllerRole_LeftHand)
 rightId = openvr.VRSystem().getTrackedDeviceIndexForControllerRole(openvr.TrackedControllerRole_RightHand)
 
-print(leftId, rightId)
-for i in ids:
-    controller_status = openvr.VRSystem().getFloatTrackedDeviceProperty(i, openvr.Prop_DeviceBatteryPercentage_Float)
-    print(controller_status)
-    sys.stdout.flush()
-    time.sleep(0.01)
-overlay = openvr.IVROverlay()
-notification = openvr.IVRNotifications()
-left_battery = str(openvr.VRSystem().getFloatTrackedDeviceProperty(leftId, openvr.Prop_DeviceBatteryPercentage_Float) * 100)[0:4]
-right_battery = str(openvr.VRSystem().getFloatTrackedDeviceProperty(rightId, openvr.Prop_DeviceBatteryPercentage_Float) * 100)[0:4]
-try:
-    openvr.IVRNotifications.createNotification(notification, overlay.createOverlay(overlayKey='battery', overlayName='batteryNotification') , 0, openvr.EVRNotificationType_Persistent, "left: %s %% \n right: %s %%" % (left_battery, right_battery), openvr.EVRNotificationStyle_Application, None)
-except openvr.error_code.NotificationError_OK:
-    pass
-time.sleep(5)
-openvr.shutdown()
+
+def to_percent(value):
+    return str(value*100)[0:4]
+
+
+while True:
+    for i in ids:
+        controller_status = openvr.VRSystem().getFloatTrackedDeviceProperty(i, openvr.Prop_DeviceBatteryPercentage_Float)
+        print(controller_status)
+        sys.stdout.flush()
+        time.sleep(0.01)
+    overlay = openvr.IVROverlay()
+    notification = openvr.IVRNotifications()
+    left_battery = openvr.VRSystem().getFloatTrackedDeviceProperty(leftId, openvr.Prop_DeviceBatteryPercentage_Float)
+    right_battery = openvr.VRSystem().getFloatTrackedDeviceProperty(rightId, openvr.Prop_DeviceBatteryPercentage_Float)
+
+    if left_battery < 0.15:
+        try:
+            openvr.IVRNotifications.createNotification(notification, overlay.createOverlay(overlayKey='battery',
+                                                                                           overlayName=''),
+                                                       0, openvr.EVRNotificationType_Persistent,
+                                                       "Left Battery low: \n %s %%" % (
+                                                       to_percent(left_battery)),
+                                                       openvr.EVRNotificationStyle_Application, None)
+        except openvr.error_code.NotificationError_OK:
+            pass
+    if right_battery < 0.15:
+        try:
+            openvr.IVRNotifications.createNotification(notification, overlay.createOverlay(overlayKey='battery',
+                                                                                           overlayName=''),
+                                                       0, openvr.EVRNotificationType_Persistent,
+                                                       "Right Battery low: \n %s %%" % (
+                                                       to_percent(right_battery)),
+                                                       openvr.EVRNotificationStyle_Application, None)
+        except openvr.error_code.NotificationError_OK:
+            pass
+
+    time.sleep(60)
